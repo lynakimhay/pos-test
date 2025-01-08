5df7edb91ac8bb7b6551805883c4af4dbf4d52a6
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2025-01-08 22:13:31 +0700

    test page customer is runing

---
+++ app/(protected)/customer/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageWrapper from "@/components/page-wrapper";
+import { PageTableView } from "./listcustomer";
+import { getPaginatedCustomers } from "@/services/customerServices";
+
+
+const CustomerPage = async ({ searchParams }: { searchParams: Record<string, string> }) => {
+  const page = parseInt(searchParams.page || "1");
+  const data = await getPaginatedCustomers({ pageSize: 10, currentPage: page });
+
+  return <PageWrapper>
+    <PageTableView title="Customers" data={data} />
+  </PageWrapper>;
+};
+
+export default CustomerPage;
